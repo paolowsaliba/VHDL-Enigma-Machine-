8b1f771c1ce1b6580ee3a3c810d9e7816e326c6e
DeeplyBrinedCode <135945715+DeeplyBrinedCode@users.noreply.github.com>
2026-03-05 11:33:30 -0800

    Add files via upload

--- EnigmaGears.vhd
+++ EnigmaGears.vhd
@@ -7,10 +7,11 @@ USE IEEE.NUMERIC_STD.ALL;
 USE WORK.MY_PACKAGE.ALL;
 ------------------------------------------------------------------------------
 ENTITY EnigmaGears IS
-	PORT(input: IN CHARACTER RANGE 'A' TO 'Z';
+	PORT(clk : IN STD_LOGIC;
+		input: IN character_az;
 		gear_order: IN gear_indices;
-		gear_pos: INOUT gear_positions;
-		output: OUT CHARACTER RANGE 'A' TO 'Z');
+		gear_pos: BUFFER gear_positions;
+		output: OUT character_az);
 END;
 ------------------------------------------------------------------------------
 ARCHITECTURE arch OF EnigmaGears IS
@@ -31,15 +32,28 @@ ARCHITECTURE arch OF EnigmaGears IS
 	TYPE gear_array IS ARRAY(0 TO 3) OF gear;
 	CONSTANT gears: gear_array:=(I, II, III, reflector);
 BEGIN
-	PROCESS(input, gear_order)
-		VARIABLE tc1, tc2, tc3, tc4, tc5, tc6: CHARACTER RANGE 'A' TO 'Z';
+	PROCESS(input, gear_order, gear_pos, clk)
+		VARIABLE tc1, tc2, tc3, tc4, tc5, tc6: character_az;
 	BEGIN
-		tc1 := gears(gear_order(0))(CHARACTER'POS(input));
-		tc2 := gears(gear_order(1))(CHARACTER'POS(tc1));
-		tc3 := gears(gear_order(2))(CHARACTER'POS(tc2));
-		tc4 := gears(3)(CHARACTER'POS(tc3));
-		tc5 := indexof(tc4, gears(gear_order(2)));
-		tc6 := indexof(tc4, gears(gear_order(1)));
-		output <= indexof(tc4, gears(gear_order(0)));
+		IF RISING_EDGE(clk) THEN
+		tc1 := gears(gear_order(0))((CHARACTER'POS(input)-65+gear_pos(0))mod 26);
+		tc2 := gears(gear_order(1))((CHARACTER'POS(tc1)-65+gear_pos(1))mod 26);
+		tc3 := gears(gear_order(2))((CHARACTER'POS(tc2)-65+gear_pos(2))mod 26);
+		tc4 := gears(3)((CHARACTER'POS(tc3)-65+gear_pos(0))mod 26);
+		tc5 := indexof(tc4, gears(gear_order(2)), gear_pos(2));
+		tc6 := indexof(tc5, gears(gear_order(1)), gear_pos(1));
+		output <= indexof(tc6, gears(gear_order(0)), gear_pos(0));
+		gear_pos(0) <= gear_pos(0)+1;
+		IF gear_pos(0) = 25 THEN
+			gear_pos(1) <= (gear_pos(1) + 1)MOD 26;
+		ELSE
+			gear_pos(1) <= gear_pos(1);
+		END IF;
+		IF (gear_pos(1) = 25 AND gear_pos(0) = 25) THEN
+			gear_pos(2) <= (gear_pos(2) + 1)MOD 26;
+		ELSE
+			gear_pos(2) <= gear_pos(2);
+		END IF;
+		END IF;
 	END PROCESS;
 END;
--- my_package.vhd
+++ my_package.vhd
@@ -3,20 +3,21 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 PACKAGE my_package IS
 	SUBTYPE character_az IS CHARACTER RANGE 'A' TO 'Z';
-	TYPE gear IS ARRAY (CHARACTER'POS('A') TO CHARACTER'POS('Z')) OF CHARACTER RANGE 'A' TO 'Z';
+	SUBTYPE integer_az IS INTEGER RANGE 0 TO 25;
+	TYPE gear IS ARRAY (0 to 25) OF character_az;
 	TYPE gear_indices IS ARRAY(0 TO 2) OF INTEGER RANGE 0 TO 2;
-	TYPE gear_positions IS ARRAY(0 TO 2) OF INTEGER RANGE 0 TO 25;
+	TYPE gear_positions IS ARRAY(0 TO 2) OF integer_az;
 	
-	FUNCTION indexof (char : character_az; arr : gear) RETURN character_az;
+	FUNCTION indexof (char : character_az; arr : gear; position : integer_az) RETURN character_az;
 END;
 
 PACKAGE BODY my_package IS
-	FUNCTION indexof (char : character_az; arr : gear) RETURN character_az IS
+	FUNCTION indexof (char : character_az; arr : gear; position : integer_az) RETURN character_az IS
 		VARIABLE result: character_az := 'A';
 	BEGIN
 		FOR i IN arr'range LOOP
-			IF arr(i) = char THEN
-				result := CHARACTER'VAL(i);
+			IF arr((i+position)mod 26) = char THEN
+				result := CHARACTER'VAL(i+65);
 				EXIT;
 				END IF;
 		END LOOP;
