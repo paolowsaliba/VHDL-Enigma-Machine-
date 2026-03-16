7db4d820fb03cd100d9ed2143a1b331a88fb87ee
DeeplyBrinedCode <135945715+DeeplyBrinedCode@users.noreply.github.com>
2026-03-15 19:50:42 -0700

    Add files via upload

--- EnigmaGears.vhd
+++ EnigmaGears.vhd
@@ -10,58 +10,48 @@ ENTITY EnigmaGears IS
 	PORT(input: IN STD_LOGIC_VECTOR(4 DOWNTO 0);
 		gear_order: IN gear_indices;
 		gear_pos: IN gear_positions;
-		plugboard: IN logical_gear;
+		plugboard: IN logical_gear:= default_plugboard;
 		output: OUT STD_LOGIC_VECTOR(4 DOWNTO 0));
+		SIGNAL splugboard_input: LogicLetter;
+		SIGNAL safter_rotors: character_az;
+		SIGNAL safter_reflector: character_az;
+		SIGNAL safter_core: LogicLetter;
+		SIGNAL first_reverse: character_az;
+		SIGNAL sec_reverse: character_az;
+		SIGNAL th_reverse: character_az;
 END;
 ------------------------------------------------------------------------------
 ARCHITECTURE arch OF EnigmaGears IS
 	-- Gears are taken from I, II, and III of the Enigma I
 	-- Reflector is taken from the UKW of the German Railway (Rocket)
-	CONSTANT I: gear:=('E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 
-							'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 
-							'B', 'R', 'C', 'J');
-	CONSTANT II: gear:=('A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 
-							'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 
-							'F', 'V', 'O', 'E');
-	CONSTANT III: gear:=('B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X',	
-							'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 
-							'U', 'S', 'Q', 'O');
-	CONSTANT reflector: gear:=('Q', 'Y', 'H', 'O', 'G', 'N', 'E', 'C', 'V', 'P', 
-							'U', 'Z', 'T', 'F', 'D', 'J', 'A', 'X', 'W', 'M', 'K', 
-							'I', 'S', 'R', 'B', 'L');
-	TYPE gear_array IS ARRAY(0 TO 3) OF gear;
-	CONSTANT gears: gear_array:=(I, II, III, reflector);
-	TYPE order_internal_type IS ARRAY(0 TO 2) OF INTEGER RANGE 0 TO 31;
-	SIGNAL order_internal: order_internal_type;
-	TYPE position_internal_type IS ARRAY(0 TO 2) OF INTEGER RANGE 0 TO 25;
-	SIGNAL position_internal: position_internal_type;
 BEGIN
-	order_internal <= (TO_INTEGER(UNSIGNED(gear_order(0))), TO_INTEGER(UNSIGNED(gear_order(1))), TO_INTEGER(UNSIGNED(gear_order(2))));
-	position_internal <= (TO_INTEGER(gear_pos(0)), TO_INTEGER(gear_pos(1)), TO_INTEGER(gear_pos(2)));
-	output <= "11010" WHEN TO_INTEGER(UNSIGNED(input)) > 25 ELSE
-		STD_LOGIC_VECTOR(TO_UNSIGNED(
-		CHARACTER'POS(
-			logical_indexof(
-				indexof(
-					indexof(
-						indexof(
-							gears(3)(
-								(CHARACTER'POS(
-									gears(order_internal(2))
-									((CHARACTER'POS(
-										gears(order_internal(1))
-										((CHARACTER'POS(
-											gears(order_internal(0))
-											((TO_INTEGER(UNSIGNED(plugboard(TO_INTEGER(UNSIGNED(input)))))+position_internal(0))mod 26)
-											)-65+position_internal(1))mod 26)
-									)-65+position_internal(2))mod 26)
-								)-65+position_internal(0))mod 26
-							)
-							, gears(order_internal(2)), position_internal(2)
-						)
-						, gears(order_internal(1)), position_internal(1)
-					)
-					, gears(order_internal(0)), position_internal(0)
-				), plugboard, 0)
-		)-65, 5));
+	PROCESS(input, gear_order, gear_pos, plugboard)
+		VARIABLE order_internal: IntGearIndices;
+		VARIABLE position_internal: PositionArray;
+		VARIABLE plugboard_input: LogicLetter;
+		VARIABLE after_rotors: character_az;
+		VARIABLE after_reflector: character_az;
+		VARIABLE after_core: LogicLetter;
+		
+		VARIABLE f_rev, s_rev, t_rev : character_az;
+	BEGIN
+		order_internal := (TO_INTEGER(UNSIGNED(gear_order(0))), TO_INTEGER(UNSIGNED(gear_order(1))), TO_INTEGER(UNSIGNED(gear_order(2))));
+		position_internal := (TO_INTEGER(gear_pos(0)), TO_INTEGER(gear_pos(1)), TO_INTEGER(gear_pos(2)));
+		plugboard_input := pass_through_plugboard(input, plugboard);
+		after_rotors := forward_through_rotors(plugboard_input, order_internal, position_internal);
+		after_reflector := pass_through_reflector(after_rotors, gears(3));
+		after_core := reverse_through_rotors(after_reflector, order_internal, position_internal);
+		output <= pass_through_plugboard(after_core, plugboard);
+		
+		splugboard_input <= plugboard_input;
+		safter_rotors <= after_rotors;
+		safter_reflector <= after_reflector;
+		safter_core <= after_core;
+		first_reverse <= indexof(after_reflector, gears(2), position_internal(2));
+		f_rev := indexof(after_reflector, gears(2), position_internal(2));
+		sec_reverse <= indexof(f_rev, gears(1), position_internal(1));
+		s_rev := indexof(f_rev, gears(1), position_internal(1));
+		th_reverse <= indexof(s_rev, gears(0), position_internal(0));
+		t_rev := indexof(s_rev, gears(0), position_internal(0));
+	END PROCESS;
 END;
--- my_package.vhd
+++ my_package.vhd
@@ -5,9 +5,12 @@ USE IEEE.NUMERIC_STD.ALL;
 PACKAGE my_package IS
 	SUBTYPE CharIndex IS INTEGER RANGE 0 TO 31;
 	SUBTYPE PositionIndex IS INTEGER RANGE 0 TO 31;
+	TYPE PositionArray IS ARRAY(0 TO 2) OF PositionIndex;
+	SUBTYPE LogicLetter IS STD_LOGIC_VECTOR(4 DOWNTO 0);
 	
 	TYPE gear_indices IS ARRAY(0 TO 2) OF STD_LOGIC_VECTOR(1 DOWNTO 0);
 	TYPE gear_positions IS ARRAY(0 TO 2) OF UNSIGNED(4 DOWNTO 0);
+	TYPE IntGearIndices IS ARRAY(0 TO 2) OF INTEGER RANGE 0 TO 3;
 	
 	TYPE logical_gear IS ARRAY(0 TO 25) OF STD_LOGIC_VECTOR(4 DOWNTO 0);
 	
@@ -16,6 +19,7 @@ PACKAGE my_package IS
 	SUBTYPE character_az IS CHARACTER RANGE 'A' TO 'Z';
 	SUBTYPE integer_az IS INTEGER RANGE 0 TO 3;
 	TYPE gear IS ARRAY (0 to 25) OF character_az;
+	TYPE GearArray IS ARRAY(0 TO 3) OF gear;
 	
 	TYPE string_az IS ARRAY (0 TO 20) OF character_az;
 	TYPE az_disp_logic_arr IS ARRAY (0 TO 20) OF CHARACTER RANGE 'A' TO '[';
@@ -25,32 +29,76 @@ PACKAGE my_package IS
 															  "01100", "01101", "01110", "01111", "10000", "10001", 
 															  "10010", "10011", "10100", "10101", "10110", "10111",
 															  "11000", "11001");
-
+	CONSTANT NUM_ROTORS: NATURAL:= 3;
+	
+	CONSTANT I: gear:=('E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 
+							'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 
+							'B', 'R', 'C', 'J');
+	CONSTANT II: gear:=('A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 
+							'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 
+							'F', 'V', 'O', 'E');
+	CONSTANT III: gear:=('B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X',	
+							'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 
+							'U', 'S', 'Q', 'O');
+	CONSTANT reflector: gear:=('Q', 'Y', 'H', 'O', 'G', 'N', 'E', 'C', 'V', 'P', 
+							'U', 'Z', 'T', 'F', 'D', 'J', 'A', 'X', 'W', 'M', 'K', 
+							'I', 'S', 'R', 'B', 'L');
+							
+	CONSTANT gears: GearArray:=(I, II, III, reflector);
+	
+	
 	FUNCTION indexof (char : character_az; arr : gear; position : PositionIndex) RETURN character_az;
-	FUNCTION logical_indexof (char : character_az; arr : logical_gear; position : PositionIndex) RETURN character_az;
+	FUNCTION forward_through_rotors (char : LogicLetter; gear_order : IntGearIndices; positions : PositionArray) RETURN character_az;
+	FUNCTION pass_through_reflector (char : character_az; arr : gear) RETURN character_az;
+	FUNCTION reverse_through_rotors (char : character_az; gear_order : IntGearIndices; positions : PositionArray) RETURN LogicLetter;
+	FUNCTION pass_through_plugboard (char : LogicLetter; arr : logical_gear) RETURN LogicLetter;
 END;
 
 PACKAGE BODY my_package IS
 	FUNCTION indexof (char : character_az; arr : gear; position : PositionIndex) RETURN character_az IS
 		VARIABLE result: character_az := 'A';
+		VARIABLE shifted_idx: INTEGER RANGE 0 TO 25 := 0;
 	BEGIN
-		FOR i IN arr'range LOOP
-			IF arr((i+position)mod 26) = char THEN
-				result := CHARACTER'VAL(i+position+65);
+		FOR char_idx IN arr'range LOOP
+			shifted_idx := (char_idx+position)MOD 26;
+			IF char = arr(shifted_idx) THEN
+				result := CHARACTER'VAL(shifted_idx-position+65);
 				EXIT;
-				END IF;
+			END IF;
 		END LOOP;
 		RETURN result;
 	END FUNCTION;
-	FUNCTION logical_indexof (char : character_az; arr : logical_gear; position : PositionIndex) RETURN character_az IS
-		VARIABLE result: character_az := 'A';
+	
+	FUNCTION forward_through_rotors (char : LogicLetter; gear_order : IntGearIndices; positions : PositionArray) RETURN character_az IS
+		VARIABLE current: character_az := 'A';
+		VARIABLE shifted_idx: INTEGER RANGE 0 TO 25 := 0;
 	BEGIN
-		FOR i IN arr'range LOOP
-			IF arr((i+position)mod 26) = STD_LOGIC_VECTOR(TO_UNSIGNED(CHARACTER'POS(char)-65, 5)) THEN
-				result := CHARACTER'VAL((i+position)MOD 26 + 65);
-				EXIT;
-				END IF;
+		current := CHARACTER'VAL(TO_INTEGER(UNSIGNED(char)) + CHARACTER'POS('A'));
+		FOR rotor_idx IN 0 TO NUM_ROTORS-1 LOOP
+			shifted_idx := (CHARACTER'POS(current)-65+positions(rotor_idx))MOD 26;
+			current := gears(gear_order(rotor_idx))(shifted_idx);
 		END LOOP;
-		RETURN result;
+		RETURN current;
+	END FUNCTION;
+	
+	FUNCTION pass_through_reflector (char : character_az; arr : gear) RETURN character_az IS
+	BEGIN
+		RETURN arr(CHARACTER'POS(char)-65);
+	END FUNCTION;
+	
+	FUNCTION reverse_through_rotors (char : character_az; gear_order : IntGearIndices; positions : PositionArray) RETURN LogicLetter IS
+		VARIABLE current: character_az := 'A';
+		VARIABLE shifted_idx: INTEGER RANGE 0 TO 25 := 0;
+	BEGIN
+		current := char;
+		FOR rotor_idx IN NUM_ROTORS-1 DOWNTO 0 LOOP
+			current := indexof(current, gears(gear_order(rotor_idx)), positions(rotor_idx));
+		END LOOP;
+		RETURN STD_LOGIC_VECTOR(TO_UNSIGNED(CHARACTER'POS(current)-65, 5));
+	END FUNCTION;
+	
+	FUNCTION pass_through_plugboard (char : LogicLetter; arr : logical_gear) RETURN LogicLetter IS
+	BEGIN
+		RETURN arr(TO_INTEGER(UNSIGNED(char)));
 	END FUNCTION;
 END;
